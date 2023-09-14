48f5544528ea25bef5a38b4e971db24a618189e9
aspmap <aspmap@yandex.ru>
2023-09-14 20:26:50 +0300

    MEDIAAPP-1 Создать и настроить проект

---
+++ src/main/java/run/itlife/mediaapp/controllers/api/test/TestApp.java
@@ -0,0 +1,18 @@
+package run.itlife.mediaapp.controllers.api.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestApp {
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping
+    public String getNameApplication() {
+        return name;
+    }
+}
