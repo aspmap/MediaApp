c869279951a11f74345eb6982734ca126633a85f
aspmap <aspmap@yandex.ru>
2023-09-20 21:23:15 +0300

    MEDIAAPP-6 Решить проблему с CORS

--- src/main/java/run/itlife/mediaapp/MediaappApplication.java
+++ src/main/java/run/itlife/mediaapp/MediaappApplication.java
@@ -2,6 +2,10 @@ package run.itlife.mediaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class MediaappApplication {
@@ -9,6 +13,17 @@ public class MediaappApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(MediaappApplication.class);
 		app.run(args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/persons").allowedOrigins("*");
+				registry.addMapping("/api").allowedOrigins("*");
+			}
+		};
 	}
 
 }
