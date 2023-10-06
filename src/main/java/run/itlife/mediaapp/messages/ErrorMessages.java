74bce0988a80365f5445cd3f1c1d6cbaf96b7594
aspmap <aspmap@yandex.ru>
2023-10-06 03:24:31 +0300

    MEDIAAPP-11 Попробовать при ошибке в return вернуть сообщение

--- src/main/java/run/itlife/mediaapp/messages/ErrorMessages.java
+++ src/main/java/run/itlife/mediaapp/messages/ErrorMessages.java
@@ -8,7 +8,6 @@ public class ErrorMessages {
     public static String DEBUG = "Debug: ";
     public static String NOT_FOUND = " not found";
     public static String OBJECT_NOT_FOUND = "Object with ID=";
-    public static String OBJECTS_NOT_FOUND = "Objects with ID=";
     public static String OBJECTS_NOT_FOUND_WORK_ID = "Objects with workId=";
     public static String OBJECTS_NOT_FOUND_PROJECT_ID = "Objects with projectId=";
     public static String OBJECTS_NOT_FOUND_PERSON_ID = "Objects with personId=";
